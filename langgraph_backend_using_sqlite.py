d80e7b7650ce4ca9e9ad731640ee7f04217b36e1
Vikash K <vikash.datascience.007@gmail.com>
2026-01-28 22:04:37 +0530

    code

---
+++ langgraph_backend_using_sqlite.py
@@ -0,0 +1,56 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_community.chat_models import ChatOllama
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages:Annotated[list[BaseMessage], add_messages]
+
+llm=ChatOllama(model="mistral:latest",temperature=0.2)
+
+def chat_node(state:ChatState):
+
+    # take user uery state
+    messages=state['messages']
+
+    # send to llm
+    response=llm.invoke(messages)
+
+    # response store state
+    return {'messages':[response]}
+
+conn=sqlite3.connect(database='chatbpt.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+CONFIG = {'configurable': {'thread_id': 'thread_id'}}
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_thread_form_db():
+    all_thread=set()
+    thread_list=checkpointer.list(None)
+
+    for thr in thread_list:
+        all_thread.add(thr.config['configurable']['thread_id'])
+    
+    return list(all_thread)
+
+# thread_id = "1"
+
+# res=chatbot.invoke(
+#                 {'messages': [HumanMessage(content='hi what iis usa')]},
+#                 config= CONFIG
+#             )
+
+# print(res)
